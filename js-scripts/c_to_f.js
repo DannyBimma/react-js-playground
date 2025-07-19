343d9402b6e3c0b0343dedc7863cbdbbc8143f66
DannyBimma <dantrotman92@gmail.com>
2025-07-19 19:59:54 -0400

    Add temp conversion program.

---
+++ js-scripts/c_to_f.js
@@ -0,0 +1,30 @@
+/*Copyright (c) 2025 DannyBimma. All Rights Reserved.
+ *
+ * The Temp Conversion ANSI-C Program Translated to JS
+ *
+ */
+
+function celsiusToFahrenheit(celsius) {
+  const fahrenheit = (celsius * 9.0) / 5.0 + 32.0;
+
+  console.log(
+    `${celsius.toString().padStart(3)} c\t=\t${fahrenheit.toFixed(1).padStart(6)} f`,
+  );
+  return fahrenheit;
+}
+
+function main() {
+  const increment = 2.5;
+  const lowerBound = 0.0;
+  const upperBound = 300.0;
+
+  // Output heading
+  console.log("CELSIUS TO FAHRENHEIT TABLE");
+  console.log("");
+
+  for (let c = lowerBound; c <= upperBound; c += increment) {
+    celsiusToFahrenheit(c);
+  }
+}
+
+main();
